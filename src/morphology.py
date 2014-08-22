8520d7dc75ed7f4454dfe9f92da80ce3efe7abdd
Martin Jul <martin@mjul.com>
2014-08-22 12:16:01 +0200

    Added custom kernel morphology example

--- src/morphology.py
+++ src/morphology.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-'''
+"""
 Code for image morphology operations.
-'''
+"""
 
 import cv2
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -20,31 +19,65 @@ def morphology(gray):
     
     for kname, kernel in kernels:
         plt.figure()
-        ax_image = plt.subplot(321)
-        plt.imshow(matplotlib_image(gray))
-        plt.title('Input')
-    
+        add_image(321, gray, 'Input')
+
         eroded = cv2.erode(gray, kernel=kernel)
-        plt.subplot(323)
-        plt.imshow(matplotlib_image(eroded))
-        plt.title('Eroded ' + kname)
-        
+        add_image(323, eroded, 'Eroded ' + kname)
+
         dilated = cv2.dilate(gray, kernel=kernel)
-        plt.subplot(324)
-        plt.imshow(matplotlib_image(dilated))
-        plt.title('Dilated '+ kname)
+        add_image(324, dilated, 'Dilated ' + kname)
 
         opened = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
-        plt.subplot(325)
-        plt.imshow(matplotlib_image(opened))
-        plt.title('Opened '+ kname)
+        add_image(325, opened, 'Opened ' + kname)
 
         closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
-        plt.subplot(326)
-        plt.imshow(matplotlib_image(closed))
-        plt.title('Closed ' + kname)
-    
+        add_image(326, closed, 'Closed ' + kname)
+
         plt.show(block=False)
+
+
+def add_image(pos, img, title):
+    plt.subplot(pos)
+    plt.imshow(matplotlib_image(img))
+    plt.title(title)
+
+
+def custom_kernels(gray):
+    """Morphological operations with custom kernels."""
+
+    fat_backstroke = np.array([[1, 1, 1, 0, 0, 0, 0, 0, 0],
+                               [0, 1, 1, 1, 0, 0, 0, 0, 0],
+                               [0, 0, 1, 1, 1, 0, 0, 0, 0],
+                               [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                               [0, 0, 0, 0, 1, 1, 1, 0, 0],
+                               [0, 0, 0, 0, 0, 1, 1, 1, 0],
+                               [0, 0, 0, 0, 0, 0, 1, 1, 1]], np.uint8)
+
+    fat_intersection = np.array([[1, 1, 1, 0, 0, 0, 1, 1, 1],
+                                 [0, 1, 1, 1, 0, 1, 1, 1, 0],
+                                 [0, 0, 1, 1, 1, 1, 1, 0, 0],
+                                 [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                                 [0, 0, 1, 1, 1, 1, 1, 0, 0],
+                                 [0, 1, 1, 1, 0, 1, 1, 1, 0],
+                                 [1, 1, 1, 0, 0, 0, 1, 1, 1]], np.uint8)
+    
+    plt.figure()
+    add_image(421, gray, 'Input')
+
+    add_image(423, 255*fat_backstroke, 'Fat Backstroke kernel')
+    add_image(424, 255*fat_intersection, 'Fat Intersection kernel')
+
+    eroded = cv2.erode(gray, kernel=fat_backstroke)
+    add_image(425, eroded, 'Backstroke eroded')
+    eroded = cv2.erode(gray, kernel=fat_intersection)
+    add_image(426, eroded, 'Intersection eroded')
+
+    dilated = cv2.dilate(gray, kernel=fat_backstroke)
+    add_image(427, dilated, 'Backstroke dilated')
+    dilated = cv2.dilate(gray, kernel=fat_intersection)
+    add_image(428, dilated, 'Intersection dilated')
+
+    plt.show()
 
 
 def matplotlib_image(image):
@@ -62,3 +95,7 @@ if  __name__ == '__main__':
     gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
     plt.close('all')
     morphology(gray)
+
+    symbols = cv2.imread('../images/symbols.png')
+    gray = cv2.cvtColor(symbols, cv2.COLOR_BGR2GRAY)
+    custom_kernels(gray)
